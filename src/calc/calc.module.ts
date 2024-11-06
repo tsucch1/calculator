445b9788e417da14aa1e51b17855ba916760756d
tsuchida <tsuchida.shimadai@gmail.com>
2024-11-06 21:01:25 +0900

    moduleの修正

--- src/calc/calc.module.ts
+++ src/calc/calc.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CalcController } from './calc.controller';
 import { CalcService } from './calc.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Calc } from 'src/entities/calc.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Calc])],
